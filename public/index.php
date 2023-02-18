e2185983d5e49996fe41bf3e905c436e0efa5e89
punkrock43 <razvan_popus@yahoo.ro>
2023-02-18 14:51:08 +0200

    multiple changes, removed unecessary js import.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Transilvanian Brew - Coffee Shop HTML Template</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name = "description" content = "">
