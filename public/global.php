c154798a795553cbf1ae46f91e4ff0c49b95ac13
punkrock43 <razvan_popus@yahoo.ro>
2023-02-18 14:38:19 +0200

    made multiple changes, converted everything to php and removed useless stuff.

---
+++ public/global.php
@@ -0,0 +1,13 @@
+<?php
+
+    $base = "https://transilvanian-brew.herokuapp.com";
+    $url = $_SERVER['REQUEST_URI'];
+
+    $lang = "ro";
+
+    if (strpos($url, 'index.php') !== false || strpos($url, 'index.html') !== false) {
+        $url = str_replace("index.php", "", $url);
+        $url = str_replace("index.html", "", $url);
+    }
+
+?>
---
+++ public/head.php
@@ -0,0 +1,15 @@
+<?php ?>
+<base href="<?php echo $base; ?>">
+<!-- Favicon -->
+<link href="img/favicon.ico" rel="icon">
+<!-- Google Font -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+<!-- Libraries Stylesheet -->
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/style.min.css" rel="stylesheet">
+<meta name = "robots" content = "noindex,nofollow">
